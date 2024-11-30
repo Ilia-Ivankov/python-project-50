1e583614e823c5771168f897ab04361fd0cedcdc
tofifi <ilaivankov919@gmail.com>
2024-11-30 13:08:19 +0300

    add tests

--- tests/expectations/stylish.py
+++ tests/expectations/stylish.py
@@ -7,47 +7,49 @@ expectation1 = '''{
   + verbose: true
 }'''
 
-expectation2 = """{
-    common: {
-      + follow: false
-        setting1: Value 1
-      - setting2: 200
-      - setting3: true
-      + setting3: null
-      + setting4: blah blah
-      + setting5: {
-            key5: value5
-        }
-        setting6: {
-            doge: {
-              - wow: 
-              + wow: so much
-            }
-            key: value
-          + ops: vops
-        }
-    }
-    group1: {
-      - baz: bas
-      + baz: bars
-        foo: bar
-      - nest: {
-            key: value
-        }
-      + nest: str
-    }
-  - group2: {
-        abc: 12345
-        deep: {
-            id: 45
-        }
-    }
-  + group3: {
-        deep: {
-            id: {
-                number: 45
-            }
-        }
-        fee: 100500
-    }
-}"""
+expectation2 = (
+    "{\n"
+    "    common: {\n"
+    "      + follow: false\n"
+    "        setting1: Value 1\n"
+    "      - setting2: 200\n"
+    "      - setting3: true\n"
+    "      + setting3: null\n"
+    "      + setting4: blah blah\n"
+    "      + setting5: {\n"
+    "            key5: value5\n"
+    "        }\n"
+    "        setting6: {\n"
+    "            doge: {\n"
+    "              - wow: \n"
+    "              + wow: so much\n"
+    "            }\n"
+    "            key: value\n"
+    "          + ops: vops\n"
+    "        }\n"
+    "    }\n"
+    "    group1: {\n"
+    "      - baz: bas\n"
+    "      + baz: bars\n"
+    "        foo: bar\n"
+    "      - nest: {\n"
+    "            key: value\n"
+    "        }\n"
+    "      + nest: str\n"
+    "    }\n"
+    "  - group2: {\n"
+    "        abc: 12345\n"
+    "        deep: {\n"
+    "            id: 45\n"
+    "        }\n"
+    "    }\n"
+    "  + group3: {\n"
+    "        deep: {\n"
+    "            id: {\n"
+    "                number: 45\n"
+    "            }\n"
+    "        }\n"
+    "        fee: 100500\n"
+    "    }\n"
+    "}"
+)
